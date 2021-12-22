4a1ea023458f87c18a0bddf78643b48deecffc34
ChigurIgor <chigur.ig@gmail.com>
2021-12-22 19:22:43 +0200

    replaced promises with async/await in Api Calls // weatherActions.js

--- src/redux/actions/locationActions.js
+++ src/redux/actions/locationActions.js
@@ -5,9 +5,6 @@ export const getCurrentLocation = () => {
         if ("geolocation" in navigator) {
             console.log("Available");
             navigator.geolocation.getCurrentPosition(function(position) {
-                // console.log("Latitude is :", position.coords.latitude);
-                // console.log("Longitude is :", position.coords.longitude);
-                // console.log("position :", position);
                 dispatch(setLocation({lat: position.coords.latitude, lon: position.coords.longitude}));
             });
         } else {
--- src/redux/actions/weatherActions.js
+++ src/redux/actions/weatherActions.js
@@ -3,70 +3,93 @@ import {API_KEY, BASE_URL_ONE_CALL, BASE_URL_WEATHER} from "../constants";
 import {setErrorAction, setForecastAction, setWeatherAction} from "./actions";
 
 export const getWeather = (city, setCity) =>{
-    return dispatch => {
-        const params = {
-            appid: API_KEY,
-            q: city,
-            lang: 'en',
-            // lat: '32.109333',
-            // lon: '34.855499',
-            unit: 'metric', // values are (metric, standard, imperial)
-        };
-         axios.get(BASE_URL_WEATHER, {params})
-             .then(response=> {
-                 setCity('');
-                 dispatch(setWeatherAction(response.data));
-             })
-             .catch(err => {
-                 dispatch(setErrorAction(err));
-             });
+    return async dispatch => {
+        try {
+            const params = {
+                appid: API_KEY,
+                q: city,
+                lang: 'en',
+                unit: 'metric', // values are (metric, standard, imperial)
+            };
+            // axios.get(BASE_URL_WEATHER, {params})
+            //     .then(response=> {
+            //         setCity('');
+            //         dispatch(setWeatherAction(response.data));
+            //     })
+            //     .catch(err => {
+            //         dispatch(setErrorAction(err));
+            //     });
+
+            const response = axios.get(BASE_URL_WEATHER, {params})
+            const {data} = await response;
+            setCity('');
+            dispatch(setWeatherAction(data))
+        }
+        catch (err){
+            dispatch(setErrorAction(err))
+        }
+
     }
 }
 export const getWeatherInCurrentLocation = (location) =>{
-    return dispatch => {
-        if(location !== undefined && location.lat !== undefined && location.lon !== undefined){
-            const params = {
-                appid: API_KEY,
-                // q: city,
-                lang: 'en',
-                lat: location.lat,
-                lon: location.lon,
-                unit: 'metric', // values are (metric, standard, imperial)
-            };
-            axios.get(BASE_URL_WEATHER, {params})
-                .then(response=> {
-                    dispatch(setWeatherAction(response.data));
-                })
-                .catch(err => {
-                    dispatch(setErrorAction(err));
-                });
-        }
-    }
-}
-
-
-export const getForecast = (weather) =>{
-        return dispatch => {
-            if(weather !== undefined && weather.coord !== undefined) {
-                const lat = weather.coord.lat;
-                const lon = weather.coord.lon
+    return async dispatch => {
+        try {
+            if (location !== undefined && location.lat !== undefined && location.lon !== undefined) {
                 const params = {
                     appid: API_KEY,
                     // q: city,
                     lang: 'en',
-                    lat: lat,
-                    lon: lon,
+                    lat: location.lat,
+                    lon: location.lon,
                     unit: 'metric', // values are (metric, standard, imperial)
                 };
-
-                axios.get(BASE_URL_ONE_CALL, {params})
-                .then(response => {
-                    dispatch(setForecastAction(response.data));
-                })
-                .catch(err => {
-                    dispatch(setErrorAction(err));
-                });
+                // axios.get(BASE_URL_WEATHER, {params})
+                //     .then(response => {
+                //         dispatch(setWeatherAction(response.data));
+                //     })
+                //     .catch(err => {
+                //         dispatch(setErrorAction(err));
+                //     });
+                const response = axios.get(BASE_URL_WEATHER, {params});
+                const {data} = await response;
+                dispatch(setWeatherAction(data));
+            }
         }
+        catch (err) {
+            setErrorAction(err)
+        }
+    }
+}
+export const getForecast = (weather) =>{
+        return async dispatch => {
+            try {
+                if (weather !== undefined && weather.coord !== undefined) {
+                    const lat = weather.coord.lat;
+                    const lon = weather.coord.lon
+                    const params = {
+                        appid: API_KEY,
+                        // q: city,
+                        lang: 'en',
+                        lat: lat,
+                        lon: lon,
+                        unit: 'metric', // values are (metric, standard, imperial)
+                    };
+
+                    // axios.get(BASE_URL_ONE_CALL, {params})
+                    //     .then(response => {
+                    //         dispatch(setForecastAction(response.data));
+                    //     })
+                    //     .catch(err => {
+                    //         dispatch(setErrorAction(err));
+                    //     });
+                    const response = axios.get(BASE_URL_ONE_CALL,{params});
+                    const {data} = await response;
+                    dispatch(setForecastAction(data));
+                }
+            }
+            catch (err){
+                setErrorAction(err)
+            }
     }
 }
 
