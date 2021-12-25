6eb2cd696eefcbe6fd2f1d0fa9b0ae4d2bbe9661
ChigurIgor <chigur.ig@gmail.com>
2021-12-25 20:34:58 +0200

    working on favouritesItem

--- src/components/favourites/favouritesItem/FavouritesItem.js
+++ src/components/favourites/favouritesItem/FavouritesItem.js
@@ -1,8 +1,13 @@
+import styles from './styles.module.css'
 const FavouritesItem = props =>{
     const {city, country} = props;
 
     return(
-    <p>{city}/{country}</p>
+        <div className={styles.itemWrapper}>
+            <span className={styles.cityName}>{city}</span>
+            <span className={styles.countryName}>{country}</span>
+            <div className={styles.btn}>Delete</div>
+        </div>
     )
 }
 
