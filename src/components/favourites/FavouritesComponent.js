50123f85f76561415927bfbbe0559f985ec08266
ChigurIgor <chigur.ig@gmail.com>
2021-12-25 21:20:09 +0200

    working on favouritesItem/ delete item

--- src/components/favourites/FavouritesComponent.js
+++ src/components/favourites/FavouritesComponent.js
@@ -1,17 +1,32 @@
 import {useLocalStorage} from "../../redux/hooks/LocalStorageHooks";
 import FavouritesItem from "./favouritesItem/FavouritesItem";
 import styles from './styles.module.css'
+import { useCallback } from 'react';
 
 const FavouritesComponent = () => {
 
     const [favourites, setFavourites] = useLocalStorage('favourites', [])
     console.log(favourites);
+    const deleteItem = (city, country) => {
+        const newArr = favourites.filter(e => (e.city !== city && e.country !== country));
+        // setFavourites(newArr)
+        console.log(newArr)
+    }
+
+    const onItemClick  = useCallback(() => {
+        console.log('Clicked!');
+    }, []);
 
     const renderList = (arr) => {
         return (
             <div>
                 {arr.map(item => <div key={item.city+"/"+item.country}>
-                    <FavouritesItem city={item.city} country = {item.country}/>
+                    <FavouritesItem
+                        city={item.city}
+                        country = {item.country}
+                        // deleteFunction ={() => deleteItem}
+                        onItemClick={deleteItem}
+                    />
                 </div>)}
             </div>
         )
--- src/components/favourites/favouritesItem/FavouritesItem.js
+++ src/components/favourites/favouritesItem/FavouritesItem.js
@@ -1,12 +1,16 @@
 import styles from './styles.module.css'
+import {useLocalStorage} from "../../../redux/hooks/LocalStorageHooks";
 const FavouritesItem = props =>{
-    const {city, country} = props;
+    const {city, country, onItemClick} = props;
 
     return(
         <div className={styles.itemWrapper}>
             <span className={styles.cityName}>{city}</span>
             <span className={styles.countryName}>{country}</span>
-            <div className={styles.btn}>Delete</div>
+            <div className={styles.btn}
+                 // onClick={() => {props.deleteItem(city, country)}}
+                 onClick={onItemClick(city, country)}
+            >Delete</div>
         </div>
     )
 }
