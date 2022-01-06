354821f9251fc2ae0243dcd3180f54c9ea46982c
ChigurIgor <chigur.ig@gmail.com>
2022-01-06 09:29:37 +0200

    working on Theme provider

--- src/components/favourites/FavouritesComponent.js
+++ src/components/favourites/FavouritesComponent.js
@@ -4,12 +4,22 @@ import styles from './styles.module.css'
 import {setSelectedCity} from "../../redux/actions/actions";
 import {useDispatch} from "react-redux";
 import {goToMainPage} from "../../redux/actions/navigationActions";
+import {useContext} from "react";
+import {ThemeContext} from "../../ThemeProvider";
+import classNames from "classnames";
 // import { useCallback } from 'react';
 
 const FavouritesComponent = () => {
     const dispatch = useDispatch();
     const [favourites, setFavourites] = useLocalStorage('favourites', [])
-
+    const {theme, setNewTheme} = useContext(ThemeContext)
+    let mainContainerTheme = '';
+    if(theme === 'cold') {
+        mainContainerTheme = styles.cold;
+    }
+    if(theme === 'warm') {
+        mainContainerTheme = styles.warm;
+    }
     const deleteItem = (city, country) => {
         const newArr = favourites.filter(e => !(e.city === city && e.country === country));
         setFavourites(newArr);
@@ -37,18 +47,18 @@ const FavouritesComponent = () => {
     }
 
     return(
-                <div className={styles.mainContainer}>
-                    <h3 className={styles.title}>Favourites</h3>
-                    {favourites !== undefined && favourites.length > 0 ?
-                    <div className={styles.listcontainer}>
+        <div className={classNames([styles.mainContainer,mainContainerTheme])}>
+            <h3 className={styles.title}>Favourites</h3>
+            {favourites !== undefined && favourites.length > 0 ?
+                <div className={styles.listContainer}>
                     {renderList(favourites)}
-                        </div>
-                        :
-                        <div className={styles.listPlaceholder}>
-                            <p>There are no saved cities yet</p>
-                        </div>
-                    }
                 </div>
+                :
+                <div className={styles.listPlaceholder}>
+                    <p>There are no saved cities yet</p>
+                </div>
+            }
+        </div>
     )
 }
 
