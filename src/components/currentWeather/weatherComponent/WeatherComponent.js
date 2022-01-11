769661f13d31ae9f783a7cd59f991c6325cc458b
ChigurIgor <chigur.ig@gmail.com>
2022-01-11 19:58:12 +0200

    fixing weather component

--- src/components/currentWeather/weatherComponent/WeatherComponent.js
+++ src/components/currentWeather/weatherComponent/WeatherComponent.js
@@ -1,17 +1,18 @@
 import styles from './styles.module.css'
 import {ZERO_TEMPERATURE} from "../../../redux/constants";
 
-const WeatherComponent = ({weather}) => {
+const WeatherComponent = ({weather, weather: {main, main: {temp, temp_min, temp_max} = {} } = {} }) => {
+
     return (
         <>
-            {weather !== undefined && weather.main !== undefined &&
+            {weather && main &&
                 <div className={styles.weatherBox}>
                     <div className={styles.tempBox}>
                         <div className={styles.temp}>
-                            {Math.round((weather.main.temp + ZERO_TEMPERATURE))}&#176;
+                            {Math.round((temp + ZERO_TEMPERATURE))}&#176;
                         </div>
                         <div className={styles.tempDaily}>
-                            {Math.round((weather.main.temp_min + ZERO_TEMPERATURE))}&#176;  {Math.round((weather.main.temp_max + ZERO_TEMPERATURE))}&#176;
+                            {Math.round((temp_min + ZERO_TEMPERATURE))}&#176;  {Math.round((temp_max + ZERO_TEMPERATURE))}&#176;
                         </div>
                         <div className={styles.weather}>
                             {weather.weather[0].main}
